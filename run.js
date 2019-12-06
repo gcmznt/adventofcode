12ca5eb92208d2806095f55195cd0a8a3c6dd83a
Giacomo Zinetti <giacomo.zinetti@gmail.com>
2019-12-06 22:40:18 +0100

    format run

--- run.js
+++ run.js
@@ -4,15 +4,13 @@ const year = process.env.YEAR
 const input = require('fs').readFileSync(`./${year}/day-${day}/input.txt`, 'utf8')
 const exercise = require(`./${year}/day-${day}/index.js`)
 
-console.log(`Day ${day} ${year}`)
-console.time('Setup')
+console.log(`  ${year}: Day ${day}`)
+console.time(' Setup')
 const run = exercise(input)
-console.timeEnd('Setup')
+console.timeEnd(' Setup')
 
 console.time('Part 1')
-const p1 = run.part1()
-console.timeLog('Part 1', p1)
+console.timeLog('Part 1', run.part1())
 
 console.time('Part 2')
-const p2 = run.part2()
-console.timeLog('Part 2', p2)
+console.timeLog('Part 2', run.part2())
